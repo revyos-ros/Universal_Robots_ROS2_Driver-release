06536cf062fd9a5b8162c934a2e19a20ee088aca
Zheng Junjie <z572@z572.online>
2025-06-17 03:59:58 +0000

    Rebase from 'release/humble/ur_controllers'

--- include/ur_controllers/passthrough_trajectory_controller.hpp
+++ include/ur_controllers/passthrough_trajectory_controller.hpp
@@ -43,9 +43,6 @@
 
 #include <stdint.h>
 
-#include <realtime_tools/realtime_buffer.h>
-#include <realtime_tools/realtime_server_goal_handle.h>
-
 #include <functional>
 #include <limits>
 #include <memory>
@@ -54,6 +51,8 @@
 #include <vector>
 
 #include <controller_interface/controller_interface.hpp>
+#include <realtime_tools/realtime_buffer.hpp>
+#include <realtime_tools/realtime_server_goal_handle.hpp>
 #include <rclcpp_action/server.hpp>
 #include <rclcpp_action/create_server.hpp>
 #include <rclcpp/rclcpp.hpp>
@@ -65,7 +64,7 @@
 #include <trajectory_msgs/msg/joint_trajectory.hpp>
 #include <control_msgs/action/follow_joint_trajectory.hpp>
 
-#include "passthrough_trajectory_controller_parameters.hpp"
+#include "ur_controllers/passthrough_trajectory_controller_parameters.hpp"
 
 namespace ur_controllers
 {
@@ -82,6 +81,7 @@ namespace ur_controllers
  * 5.0: The robot finished executing the trajectory.
  */
 const double TRANSFER_STATE_IDLE = 0.0;
+const double TRANSFER_STATE_NEW_TRAJECTORY = 6.0;
 const double TRANSFER_STATE_WAITING_FOR_POINT = 1.0;
 const double TRANSFER_STATE_TRANSFERRING = 2.0;
 const double TRANSFER_STATE_TRANSFER_DONE = 3.0;
@@ -175,6 +175,7 @@ private:
 
   std::optional<std::reference_wrapper<hardware_interface::LoanedStateInterface>> scaling_state_interface_;
   std::optional<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> abort_command_interface_;
+  std::optional<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> trajectory_size_command_interface_;
   std::optional<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> transfer_command_interface_;
   std::optional<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> time_from_start_command_interface_;
 
